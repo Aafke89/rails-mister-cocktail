8cef8cc7ff7908142d3a5dfbaee368d73d70c72e
Aafke Smal <aafke.smal@gmail.com>
2016-06-02 17:21:44 +0200

    Prepared for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses
   end
+# get '/doses/:id', to: 'doses#destroy', as: 'delete_dose'
+root to: 'cocktails#index'
 end
+
