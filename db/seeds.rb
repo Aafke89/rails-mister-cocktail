e727b177e99d9348ed43b3f0d529a34c9b379620
Aafke Smal <aafke.smal@gmail.com>
2016-06-02 12:46:24 +0200

    With cocktailmodel and controller and ingredient model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+
+6.times do
+  name = Faker::Hipster.words(2).join(" ")
+  Cocktail.create(name: name)
+end
+
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Rum")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Sparkling water")
+Ingredient.create(name: "Tonic")
+Ingredient.create(name: "Red Bull")
+Ingredient.create(name: "Mint")
+Ingredient.create(name: "Lemon")
+Ingredient.create(name: "Basil")
